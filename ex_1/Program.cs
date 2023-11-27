03fa3243f7aec9f74bd489eae280371fccac60f8
Jalil Tahirov <Jalil.Tahirov@dominos.co.uk>
2023-11-28 00:35:31 +0400

    Add project files.

---
+++ ex_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int sum(int n)
+{
+    if (n <= 0) return 0;
+    return n + sum(n - 1);
+}
+
+System.Console.WriteLine(sum(7));
